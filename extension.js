cfdbde41901f4b2bc87dc09a44696cff29955d44
Stuart Hayhurst <stuart.a.hayhurst@gmail.com>
2020-12-28 12:43:39 +0000

    Fixed type error

--- extension.js
+++ extension.js
@@ -1,34 +1,34 @@
 const Main = imports.ui.main;
+let appMenu = Main.panel.statusArea.appMenu;
 
 class Extension {
   constructor() {
-    this.appMenu=Main.panel.statusArea.appMenu;
     this.monitorsChangedEvent = null;
     this.showEvent = null;
   }
 
-  hideMenu() {
+  _hideMenu() {
     //Hide the menu if available
-    if(this.appMenu != null) {
-      this.appMenu.hide();
+    if(appMenu != null) {
+      appMenu.hide();
     }
   }
 
   enable() {
     //Hide menu when something attempts to show it or the ui is reloaded
-    this.monitorsChangedEvent = Main.layoutManager.connect('monitors-changed', this.hideMenu);
-    this.showEvent = this.appMenu.connect('show', this.hideMenu);
+    //this.monitorsChangedEvent = Main.layoutManager.connect('monitors-changed', this._hideMenu);
+    this.showEvent = appMenu.connect('show', this._hideMenu);
     //Hide appMenu
-    this.hideMenu();
+    this._hideMenu();
   }
 
   disable() {
     //Disconnect hiding the app menu from events and show it again
-    this.appMenu.disconnect(this.monitorsChangedEvent);
-    this.appMenu.disconnect(this.showEvent);
+    appMenu.disconnect(this.monitorsChangedEvent);
+    appMenu.disconnect(this.showEvent);
     //Show appMenu, if available
-    if(this.appMenu != null) {
-      this.appMenu.show();
+    if(appMenu != null) {
+      appMenu.show();
     }
   }
 }
